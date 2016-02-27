c009d89b1f33e7a0bda620b750617aadc65da6b3
dachuanz <dachuanz@gmail.com>
2016-02-27 16:17:30 +0800

    Update AggregationInvokeHandler.java

--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/merger/aggregation/AggregationInvokeHandler.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/merger/aggregation/AggregationInvokeHandler.java
@@ -1,19 +1,3 @@
-/**
- * Copyright 1999-2015 dangdang.com.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * </p>
- */
 
 package com.dangdang.ddframe.rdb.sharding.merger.aggregation;
 
@@ -51,6 +35,8 @@ public final class AggregationInvokeHandler extends AbstractMergerInvokeHandler<
             if (resultSetQueryIndex.isQueryBySequence() && each.getIndex() == resultSetQueryIndex.getQueryIndex()) {
                 return Optional.of(each);
             } else if (each.getAlias().isPresent() && each.getAlias().get().equals(resultSetQueryIndex.getQueryName())) {
+                return Optional.of(each);
+            } else if (each.getExpression().equalsIgnoreCase(resultSetQueryIndex.getQueryName())) {
                 return Optional.of(each);
             }
         }
